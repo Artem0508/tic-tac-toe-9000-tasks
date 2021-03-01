de62cf5d01811658ab457bc91dfe1c1faa17e9a1
Artem0508 <sheshelev2006@mail.ru>
2021-03-01 09:54:14 +0300

    edit files

--- game_engine/tic_tac_toe_game.py
+++ game_engine/tic_tac_toe_game.py
@@ -1,13 +1,10 @@
+from typing import Callable, List
 from copy import deepcopy
-from typing import Callable, List, Optional
-from game_engine import TicTacToeTurn, TicTacToeGameInfo, AbstractTicTacToeGame
-
+from .tic_tac_toe_common_lib import TicTacToeTurn, TicTacToeGameInfo, AbstractTicTacToeGame
 
 class TicTacToeGame(AbstractTicTacToeGame):
-    """Наследуемся от абстрактного класса и реализуем ручками все методы"""
-
     def __init__(self, game_id: str, first_player_id: str, second_player_id: str,
-                 strategy: Optional[Callable[[TicTacToeGameInfo], TicTacToeTurn]] = None) -> None:
+                 strategy: Callable[[TicTacToeGameInfo], TicTacToeTurn] = None) -> None:
         self.__game_id = game_id
         self.__first_player_id = first_player_id
         self.__second_player_id = second_player_id
@@ -16,10 +13,58 @@ class TicTacToeGame(AbstractTicTacToeGame):
         self.__turns: List[TicTacToeTurn] = []
 
     def is_turn_correct(self, turn: TicTacToeTurn) -> bool:
-        raise NotImplementedError
+        if self.__winner_id != "":
+            return False
+        if not (0 <= turn.x_coordinate <= 2 and 0 <= turn.y_coordinate <= 2):
+            return False
+        if self._current_player_id() != turn.player_id:
+            return False
+        return True
+
+    def _current_player_id(self) -> str:
+        if self.__turns == []:
+            return self.__first_player_id
+        if self.__turns[-1].player_id == self.__first_player_id:
+            return self.__second_player_id
+        return self.__first_player_id
 
     def do_turn(self, turn: TicTacToeTurn) -> TicTacToeGameInfo:
-        raise NotImplementedError
+        if self.is_turn_correct(turn):
+            self.__turns.append(deepcopy(turn))
+            self._set_winner()
+        return self.get_game_info()
+
+    def _set_winner(self) -> None:
+        field = self.get_game_info().field
+        for i in range(3):
+            row1 = ""
+            row2 = ""
+            for j in range(3):
+                row1 += field[i][j]
+                row2 += field[i][j]
+            if row1 == "XXX" or row2 == "XXX":
+                self._winner_id = self.__first_player_id
+                return
+            if row1 == "OOO" or row2 == "OOO":
+                self._winner_id = self.__first_player_id
+                return
+            if ("X" not in row1 or "X" not in row2 or "O" not in row1 or "O" not in row2):
+                draw = False
+        row1 = ""
+        row2 = ""
+        for i in range(3):
+            row1 += field[i][i]
+            row2 += field[i][2-i]
+        if row1 == "XXX" or row2 == "XXX":
+            self._winner_id = self.__first_player_id
+            return
+        if row1 == "OOO" or row2 == "OOO":
+            self._winner_id = self.__first_player_id
+            return
+        if ("X" not in row1 or "X" not in row2 or "O" not in row1 or "O" not in row2):
+            draw = False
+        if draw:
+            self._winner_id = "draw"
 
     def get_game_info(self) -> TicTacToeGameInfo:
         result = TicTacToeGameInfo(
